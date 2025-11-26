ce213d0046375ac22f800d61411f02a26d53be27
T8RIN <oolbp@bk.ru>
2025-11-26 15:05:28 +0300

    screenshots

--- composeApp/src/commonMain/kotlin/com/example/kmpapp/WeatherViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/example/kmpapp/WeatherViewModel.kt
@@ -3,9 +3,10 @@ package com.example.kmpapp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class WeatherViewModel {
@@ -13,21 +14,27 @@ class WeatherViewModel {
     private val pref = WeatherPreferences()
     private val viewModelScope = CoroutineScope(Dispatchers.Main)
 
-    val city = pref.cityFlow.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.Eagerly,
-        initialValue = ""
-    )
+    private val _city: MutableStateFlow<String> = MutableStateFlow("")
+    val city = _city.asStateFlow()
 
     private val _state = MutableStateFlow<WeatherState>(WeatherState.Loading)
     val state: StateFlow<WeatherState> = _state
 
     init {
         reload()
+
+        viewModelScope.launch {
+            pref.cityFlow.collectLatest { new ->
+                _city.update { new }
+            }
+        }
     }
 
     fun onCityChange(v: String) {
-        viewModelScope.launch { pref.setCity(v) }
+        _city.update { v }
+        viewModelScope.launch {
+            pref.setCity(v)
+        }
     }
 
     fun reload() {
